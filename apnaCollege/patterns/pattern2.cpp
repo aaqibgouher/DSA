3dff44e6efb04c51717434028c95dd5e17ee12e4
aaqibgouher <aaqibgouher@gmail.com>
2023-01-13 10:02:33 +0530

    pattern lecture completed

--- apnaCollege/patterns/pattern2.cpp
+++ apnaCollege/patterns/pattern2.cpp
@@ -117,5 +117,69 @@ int main() {
     //     cout << endl;
     // }
 
+    /*
+        6. Star pattern: n = 4
+              *
+            * * *
+          * * * * *
+        * * * * * * *
+        * * * * * * *
+          * * * * *
+            * * *
+              *
+    */
+    // int n;
+    // cout << "Enter value of n:" << endl;
+    // cin >> n;
+
+    // // upper half
+    // for(int i=1; i<=n; ++i) {
+    //     // print space
+    //     for(int j=1; j<=n-i; ++j) {
+    //         cout << "_ ";
+    //     }
+    //     // print stars
+    //     for(int j=1; j<=2*i-1; ++j) {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // // lower half
+    // for(int i=n; i>=1; --i) {
+    //     // print space
+    //     for(int j=1; j<=n-i; ++j) {
+    //         cout << "_ ";
+    //     }
+    //     // stars
+    //     for(int j=1; j<=2*i-1; ++j) {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+
+    /*
+        7. Zig-zag pattern: n = 9
+        _ _ * _ _ _ *
+        _ * _ * _ * _ *
+        * _ _ _ * _ _ _ *
+    */
+
+    // int n;
+    // cout << "Enter value of n:" << endl;
+    // cin >> n;
+
+    // for(int i=1; i<=3; ++i) {
+    //     for(int j=1; j<=9; ++j) {
+    //         if((i+j) % 4 == 0 || (i == 2 && j % 4 == 0)) {
+    //             cout << "* ";
+    //         }else {
+    //             cout << "_ ";
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+    
+
     return 0;
 }
