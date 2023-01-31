f4fed84fcd391bd1411e945dbbaca0cc5a40c32e
aaqibgouher <aaqibgouher@gmail.com>
2023-01-31 09:32:11 +0530

    completed binary search

--- apnaCollege/lecture_8_arrays/array_3.cpp
+++ apnaCollege/lecture_8_arrays/array_3.cpp
@@ -11,23 +11,21 @@ int linearSearch(int arr[], int n, int key) {
     return -1;
 }
 
-// int binarySearch(int arr[], int n, int key) {
-//     int middle = arr[0];
+int binarySearch(int arr[], int n, int key) {
+    int start = 0;
+    int end = n-1;
+    int middle = n/2;
 
-//     for(int i=0; i<n; ++i) {
-//         middle = n/2;
+    while(start <=end) {
+        middle = (start + end) / 2;
 
-//         if(key == arr[middle]) {
-//             return i;
-//         }else if(key < arr[middle]) {
-//             n /= 2;
-//         }else {
-//             i = (n / 2) + 1;
-//         }
-//     }
+        if(key == arr[middle]) return middle;
+        else if(key < arr[middle]) end = middle - 1;
+        else if(key > arr[middle]) start = middle + 1;
+    }
 
-//     return -1;
-// }
+    return -1;
+}
 
 int main() {
     // 1. Linear search in an array:
@@ -63,14 +61,23 @@ int main() {
     //     cin >> arr[i];
     // }
 
-    // int key;
+    // int n, key;
+    // cout << "Enter array size:" << endl;
+    // cin >> n;
+
+    // int arr[n];
+
+    // cout << "Enter array elements:" << endl;
+    // for(int i=0; i<n; ++i) {
+    //     cin >> arr[i];
+    // }
+
     // cout << "Enter search element:" << endl;
     // cin >> key;
 
     // int index = binarySearch(arr, n, key);
-
-    // if(index == 0 || index) cout << key << " present at " << index;
-    // else cout << key << " not present";
+    // if(index >= 0) cout << key << " is present at " << index;
+    // else cout << key << " is not present";
 
     return 0;
 }
