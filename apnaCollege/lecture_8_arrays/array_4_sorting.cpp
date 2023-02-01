1f4294e937f6c05d36c2cd62eb56ee4341cf331a
aaqibgouher <aaqibgouher@gmail.com>
2023-02-01 10:57:11 +0530

    completed bubble sort

--- apnaCollege/lecture_8_arrays/array_4_sorting.cpp
+++ apnaCollege/lecture_8_arrays/array_4_sorting.cpp
@@ -2,30 +2,55 @@
 using namespace std;
 
 int main() {
-    // 1. Selection sort: find minimum element, and swap it
-    int n, k;
-    cout << "Enter array size:" << endl;
-    cin >> n;
+    // 1. Selection sort: find minimum element, and swap it. On each step, we get our minimum element to left side. 
+    // int n, k;
+    // cout << "Enter array size:" << endl;
+    // cin >> n;
 
-    int arr[n];
-    cout << "Enter array element:" << endl;
-    for(int i=0; i<n; ++i) {
-        cin >> arr[i];
-    }
+    // int arr[n];
+    // cout << "Enter array element:" << endl;
+    // for(int i=0; i<n; ++i) {
+    //     cin >> arr[i];
+    // }
 
-    for(int i=0; i<n; ++i) {
-        for(int j=i+1; j<n; ++j) {
-            if(arr[i] > arr[j]) {
-                k = arr[i];
-                arr[i] = arr[j];
-                arr[j] = k;
-            }
-        }
-    }
+    // for(int i=0; i<n; ++i) {
+    //     for(int j=i+1; j<n; ++j) {
+    //         if(arr[i] > arr[j]) {
+    //             k = arr[i];
+    //             arr[i] = arr[j];
+    //             arr[j] = k;
+    //         }
+    //     }
+    // }
 
-    for(int i=0; i<n; ++i) {
-        cout << arr[i] << " ";
-    }
+    // for(int i=0; i<n; ++i) {
+    //     cout << arr[i] << " ";
+    // }
+
+    // 2. Bubble sort: On each step, we get our max element to right. 
+    // int n, k;
+    // cout << "Enter array size:" << endl;
+    // cin >> n;
+
+    // int arr[n];
+    // cout << "Enter array element:" << endl;
+    // for(int i=0; i<n; ++i) {
+    //     cin >> arr[i];
+    // }
+
+    // for(int i=0; i<n-1; ++i) {
+    //     for(int j=0; j<n-i-1; ++j) {
+    //         if(arr[j] > arr[j+1]) {
+    //             k = arr[j];
+    //             arr[j] = arr[j+1];
+    //             arr[j+1] = k;
+    //         }
+    //     }
+    // }
+
+    // for(int i=0; i<n; ++i) {
+    //     cout << arr[i] << " ";
+    // }
 
     return 0;
 }
