5f06b81d497bf59b23e820c1dfa4d5fc615feddf
aaqibgouher <aaqibgouher@gmail.com>
2023-02-24 09:14:10 +0530

    completed basic recurssion problem

--- apnaCollege/lecture_16_recursion/lecture_16.2_challenges.cpp
+++ apnaCollege/lecture_16_recursion/lecture_16.2_challenges.cpp
@@ -21,6 +21,41 @@ void increasing(int num) {
     
 }
 
+// first occurence checking from start
+int firstOcc(int arr[], int n, int key, int i) {
+    if(i == n) {
+        return -1;
+    }
+
+    if(arr[i] == key) {
+        return i;
+    }
+
+    return firstOcc(arr, n, key, i+1);
+}
+
+// last occurence checking from last
+int lastOcc(int arr[], int n, int key, int i) {
+    if(i == -1) return -1;
+
+    if(arr[i] == key) return i;
+
+    return lastOcc(arr, n, key, i--);
+}
+
+// last occurence checking from start
+int lastOccStart(int arr[], int n, int key, int i) {
+    if(i == n) return -1;
+
+    int lastArr = lastOccStart(arr, n, key, i+1);
+
+    if(lastArr != -1) return lastArr;
+
+    if(arr[i] == key) return i;
+
+    return -1;
+}
+
 int main() {
     /*
         1. check if an array is sorted or not:
@@ -47,6 +82,28 @@ int main() {
     // decreasing(num);
     // cout << endl;
     // increasing(num);
+
+    /*
+        3. Find the first and last occurence of a number in an array:
+        eg: {4, 2, 1, 2, 5, 2, 7}, now for 2, start = 1, and end = 5
+    */
+    // int n;
+    // cout << "Enter array size\n";
+    // cin >> n;
+
+    // int arr[n];
+    // cout << "Enter array element\n";
+    // for(int i=0; i<n; ++i) {
+    //     cin >> arr[i];
+    // }
+
+    // int key;
+    // cout << "Enter key element\n";
+    // cin >> key;
+
+    // cout << key << " first occurs at index " << firstOcc(arr, n, key, 0) << endl;
+    // // cout << key << " last occurs at index " << lastOcc(arr, n, key, n-1) << endl;
+    // cout << key << " last occurs at index " << lastOccStart(arr, n, key, 0) << endl;
 
     return 0;
 }
