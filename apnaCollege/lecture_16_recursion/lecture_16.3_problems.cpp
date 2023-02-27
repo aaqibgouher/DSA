4970a15eb4515903d36556b608a19759acac6f5b
aaqibgouher <aaqibgouher@gmail.com>
2023-02-27 14:41:45 +0530

    working on string substring using recursion

--- apnaCollege/lecture_16_recursion/lecture_16.3_problems.cpp
+++ apnaCollege/lecture_16_recursion/lecture_16.3_problems.cpp
@@ -35,6 +35,53 @@ void replacePI(string name, int len, int i) {
     replacePI(name, len, i+1);
 }
 
+void towerOfhanoi(int n, char source, char destination, char helper) {
+    if(n == 0) return;
+
+    towerOfhanoi(n-1, source, helper, destination);
+    cout << "Move from " << source << " to " << destination << endl;
+    towerOfhanoi(n-1, helper, destination, source);
+}
+
+string removeDuplicate(string str) {
+    if(str.length() == 0) return "";
+
+    char ch = str[0];
+    string restStr = removeDuplicate(str.substr(1));
+
+    if(ch == restStr[0]) {
+        return restStr;
+    }
+
+    return ch + restStr;
+}
+
+string moveXToEnd(string str) {
+    if(str.length() == 0) return "";
+
+    char ch = str[0];
+    string restStr = moveXToEnd(str.substr(1));
+
+    if(ch == 'x') {
+        return restStr + ch;
+    }
+    
+    return ch + restStr;
+}
+
+void allSubStr(string str, int i, int j) {
+    if(i == str.length()) return;
+
+    if(i == j) cout << str[i] << " ";
+    else cout << str[i] << str[j] << " ";
+
+    if(j == str.length()) {
+        allSubStr(str, i+1, i+1);
+    } else {
+        allSubStr(str, i, j+1);
+    }
+}
+
 int main() {
     /*
         1. Reverse a string using recursion:
@@ -65,13 +112,59 @@ int main() {
 
     // cout << word;
 
-    // 2nd approach using recusion
-    string word;
-    cout << "Enter your word\n";
-    cin >> word;
-    int len = word.length();
+    // 2nd approach using recusionr
+    // string word;
+    // cout << "Enter your word\n";
+    // cin >> word;
+    // int len = word.length();
 
-    replacePI(word, len-1, 0);
+    // replacePI(word, len-1, 0);
+
+    /*
+        3. Tower of hanoi: 
+    */
+    // int n;
+    // cout << "Enter number\n";
+    // cin >> n;
+
+    // towerOfhanoi(n, 'S', 'D', 'H');  // source, destination and helper
+
+    /*
+        4. Remove all duplicates from the string: 
+        string = aaaabbbcccddd, output = abcd
+    */
+    // string str;
+    // cout << "Enter string\n";
+    // cin >> str;
+    // int len = str.length();
+
+    // cout << removeDuplicate(str);
+
+    /*
+        5. Move all x to the end of the string:
+        eg: str = axxbdxcefchix, output: abdcefhixxxx
+    */
+
+    // string str;
+
+    // cout << "Enter a string\n";
+    // cin >> str;
+
+    // int len = str.length();
+
+    // cout << moveXToEnd(str);
+
+    /*
+        6. Generate all substrings of a string:
+        eg: str = ABC, 
+        output: "", "A", "B", "C", "AB", "AC", "BC", "ABC"
+    */
+    string str;
+    cout << "Enter a string\n";
+    cin >> str;
+
+    allSubStr(str, 0, 0);
+    
 
     return 0;
 }
