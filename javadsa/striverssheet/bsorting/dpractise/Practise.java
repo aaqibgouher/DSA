777fc9efb5b58e0d320fcc3a3b0a077390789d6a
aaqibgouher <aaqibgouher@gmail.com>
2024-12-04 09:33:37 +0530

    working on dsa

---
+++ javadsa/striverssheet/bsorting/dpractise/Practise.java
@@ -0,0 +1,11 @@
+public class Practise {
+    public void print(int[] arr, int n) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 12, 1, 3, 6, 2, 4 };
+        int n = arr.length;
+
+    }
+}
